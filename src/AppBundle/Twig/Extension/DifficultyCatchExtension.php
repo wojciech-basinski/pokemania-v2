b985705b6b70c3facd608c80fad953b7ff892276
demot <wojtek13radom@gmail.com>
2019-02-26 13:16:20 +0100

    catching difficulty info fixed

---
+++ src/AppBundle/Twig/Extension/DifficultyCatchExtension.php
@@ -0,0 +1,34 @@
+<?php
+namespace AppBundle\Twig\Extension;
+
+class DifficultyCatchExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('catchDifficulty', function ($difficulty) {
+                switch ($difficulty) {
+                    case 1:
+                        return 'bardzo łatwa';
+                    case 2:
+                        return 'łatwa';
+                    case 3:
+                        return 'średnia';
+                    case 4:
+                        return 'średnio trudna';
+                    case 5:
+                        return 'trudna';
+                    case 6:
+                        return 'bardzo trudna';
+                    default:
+                        return 'Niemożliwy do złapania';
+                }
+            }),
+        ];
+    }
+
+    public function getName()
+    {
+        return 'catchDifficulty';
+    }
+}
